0ca86d56e1ac3142afedd9fa84199e875bd9e34a
TeraLabDev <ebasilio.dev@gmail.com>
2020-05-28 22:57:25 -0300

    Ativiade 5 pontos JavaScript

---
+++ JavaScript.js
@@ -0,0 +1,35 @@
+let receita = {
+    id: "99",
+    nome: "Super Feijoada",
+    porcoes: 50, 
+    Ingredientes: [
+        "1 orelha de porco cortada em pedaços pequenos",
+        "2 pés de porco cortados em pedaços pequenos",
+        "1/2 kg de carne-seca cortada em pedaços pequenos",
+        "1/2 kg de costela de porco cortada em pedaços pequenos",
+        "1/2 kg de feijão preto",
+        "2 colheres de sopa de óleo",
+        "1 cebola picada",
+        "300 gramas de linguiça calabresa de porco cortados em fatias grossas",
+        "300 gramas de linguiça de paio cortada em fatias grossas",
+        "400 grama de lombo de porco sem osso cortado em cubos",
+        "1 laranja média sem casca",
+        "2 Cubos de Caldo de Bacon Arisco",
+        "2 litros de água fervente", 
+        ]
+    }
+    console.log(receita.Ingredientes[0]);
+    console.log(receita.Ingredientes[1]);
+    console.log(receita.Ingredientes[2]);
+    console.log(receita.Ingredientes[3]);
+    console.log(receita.Ingredientes[4]);
+    console.log(receita.Ingredientes[5]);
+    console.log(receita.Ingredientes[6]);
+    console.log(receita.Ingredientes[7]);
+    console.log(receita.Ingredientes[8]);
+    console.log(receita.Ingredientes[9]);
+    console.log(receita.Ingredientes[10]);
+    console.log(receita.Ingredientes[11]);
+    console.log(receita.Ingredientes[12]);
+    
+
