d1d576fc7b0ae0b33dbbf1f90dd844dea7a84730
TeraLabDev <ebasilio.dev@gmail.com>
2020-06-04 23:59:09 -0300

    Atividades JavaScript.

--- JsExe2.js
+++ JsExe2.js
@@ -1,6 +1,27 @@
-let Livro = {
-    id: 20,
-    [
+let biblioteca = {
 
-    ]
+    Proprietario: "Unibh",
+
+    var: array = [{
+        titulo: "O Escaravelho do Diabo",
+        autor: "Lúcia Machado de Almeida",
+        LivroLido: Boolean,
+    }, {
+        titulo: "Lendas da Terra do Ouro",
+        autor: "Lúcia Machado de Almeida",
+        LivroLido: Boolean,
+    }, {
+        titulo: "Xisto no Espaço",
+        autor: "Lúcia Machado de Almeida",
+        LivroLido: Boolean,
+    }, {
+        titulo: "Passeio a Ouro Preto",
+        autor: "Lúcia Machado de Almeida",
+        let LivroLido: Boolean(true),
+    }]
 }
+
+console.log(array[0]);
+console.log(array[1]);
+console.log(array[2]);
+console.log(array[3]);
