25b2a1cfc309b0bb1d62024ab240fc928950a56f
TeraLabDev <ebasilio.dev@gmail.com>
2020-06-05 00:30:57 -0300

    Listas de eercícios atvJavaScripts.

--- JsExe2.js
+++ JsExe2.js
@@ -3,25 +3,56 @@ let biblioteca = {
     Proprietario: "Unibh",
 
     var: array = [{
-        titulo: "O Escaravelho do Diabo",
-        autor: "Lúcia Machado de Almeida",
-        LivroLido: Boolean,
-    }, {
-        titulo: "Lendas da Terra do Ouro",
-        autor: "Lúcia Machado de Almeida",
-        LivroLido: Boolean,
-    }, {
-        titulo: "Xisto no Espaço",
-        autor: "Lúcia Machado de Almeida",
-        LivroLido: Boolean,
-    }, {
-        titulo: "Passeio a Ouro Preto",
-        autor: "Lúcia Machado de Almeida",
-        let LivroLido: Boolean(true),
-    }]
+            titulo: "O Escaravelho do Diabo",
+            autor: "Lúcia Machado de Almeida",
+            LivroLido: Boolean,
+        }, {
+            titulo: "Lendas da Terra do Ouro",
+            autor: "Lúcia Machado de Almeida",
+            LivroLido: Boolean,
+        }, {
+            titulo: "Xisto no Espaço",
+            autor: "Lúcia Machado de Almeida",
+            LivroLido: Boolean,
+        }, {
+            titulo: "Passeio a Ouro Preto",
+            autor: "Lúcia Machado de Almeida",
+            LivroLido: Boolean,
+        },
+        {
+            titulo: "Xisto e o Pássaro Cósmico",
+            autor: "Lúcia Machado de Almeida",
+            LivroLido: Boolean,
+        },
+        {
+            titulo: "Passeio a Sabará",
+            autor: "Lúcia Machado de Almeida",
+            LivroLido: Boolean,
+        },
+        {
+            titulo: "Passeio a Diamantina",
+            autor: "Lúcia Machado de Almeida",
+            LivroLido: Boolean,
+        },
+        {
+            titulo: "Passeio ao Alto Minho",
+            autor: "Lúcia Machado de Almeida",
+            LivroLido: Boolean,
+        },
+        {
+            titulo: "Asteróide",
+            autor: "Lúcia Machado de Almeida",
+            LivroLido: Boolean,
+        }
+    ]
 }
 
 console.log(array[0]);
 console.log(array[1]);
 console.log(array[2]);
 console.log(array[3]);
+console.log(array[4]);
+console.log(array[5]);
+console.log(array[6]);
+console.log(array[7]);
+console.log(array[8]);
