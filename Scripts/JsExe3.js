ad9bf1e7daf77d82f9e7a98dfe0b20936e41e711
TeraLabDev <ebasilio.dev@gmail.com>
2020-06-09 01:09:55 -0300

    Atividades.

--- Scripts/JsExe3.js
+++
@@ -1,6 +0,0 @@
-var teste3 = fetch('https://dog.ceo/api/breeds/image/random')
-    .then(response => response.json())
-    .then(data => {
-        console.log("   <img src=" + data.message + " alt='Random Photo of a Dog' />")
-    })
-    .catch(error => console.error(error))
